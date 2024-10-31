3705650a2c32174b74dcfc6320907034063bc79c
Iker <iker.a@opendeusto.es>
2024-10-31 08:36:50 +0100

    Add foto field to Artista model as a URLField and create migration

--- galeriaArteAPP/models.py
+++ galeriaArteAPP/models.py
@@ -8,7 +8,7 @@ class Artista(models.Model):
     fecha_fallecimiento = models.DateField(null=True, blank=True)
     biografia = models.TextField()
     nacionalidad = models.CharField(max_length=50)
-    #foto = models.URLField(blank=True, null=True)
+    foto = models.URLField(blank=True, null=True)
     
     def __str__(self):
         return self.nombre_ape
