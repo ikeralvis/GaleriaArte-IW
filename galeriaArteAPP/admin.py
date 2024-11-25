f0b78c54d0b277b0d9582e69b6c3665e019373fa
Iker <iker.a@opendeusto.es>
2024-11-25 22:34:51 +0100

    Add custom user creation form and admin integration

--- galeriaArteAPP/admin.py
+++ galeriaArteAPP/admin.py
@@ -1,9 +1,52 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite, ModelAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import Artista, Cuadro, Exposicion
 from django.contrib.auth.models import User, Group
 from django.utils.html import format_html
+from django import forms
 # Register your models here.
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmar Contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'groups')  # Los campos que queremos mostrar
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])  # Asegura que la contraseña esté encriptada
+        if commit:
+            user.is_active = True  # Asegurarse de que el usuario está activo
+            user.save()  # Guarda el usuario en la base de datos
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'groups'),
+        }),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        # Usar el formulario personalizado solo cuando estamos creando un usuario nuevo
+        if obj is None:
+            return self.add_form
+        return super().get_form(request, obj, **kwargs)
+    
+
 
 class CustomAdminSite(AdminSite):
     class Media:  
@@ -24,7 +67,7 @@ class CustomAdminSite(AdminSite):
 # Instancia el sitio de administración personalizado
 custom_admin_site = CustomAdminSite(name='custom_admin')
 
-custom_admin_site.register(User)
+custom_admin_site.register(User, CustomUserAdmin)
 custom_admin_site.register(Group)
 
 class ExposicionAdmin(admin.ModelAdmin):
