d82732df4c4075353269e81a55f537ceb501bee9
John Blanco Argollo <109868057+Ahurazda@users.noreply.github.com>
2024-11-26 01:54:14 +0100

    Reapply "Merge branch 'main' of https://github.com/ikeralvis/GaleriaArte-IW"
    This reverts commit 9a3d5e75bbc399db392b4794463676bcd263a20d.

---
+++ galeriaArteAPP/static/js/cuadros.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cuadros = document.querySelectorAll(".cuadro-item");
+    cuadros.forEach((cuadro, index) => {
+        setTimeout(() => {cuadro.classList.add("show");}, index * 150);
+    });
+});
